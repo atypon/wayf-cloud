36d916a48669bd862305569602385f0f0931c0f7
Matthew Mason <mjm@matthewjmason.com>
2017-05-04 14:56:24 -0400

    Travis env update

--- src/test/resources/create_wayf_tables.sql
+++ src/test/resources/create_wayf_tables.sql
@@ -18,6 +18,8 @@
 --
 -- Table structure for table `device`
 --
+USE DATABASE wayf;
+
 DROP TABLE IF EXISTS `device`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -25,8 +27,8 @@ CREATE TABLE `device` (
   `_id` int(11) NOT NULL AUTO_INCREMENT,
   `id` varchar(36) NOT NULL,
   `status` varchar(15) NOT NULL,
-  `created_date` timestamp NOT NULL,
-  `modified_date` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`_id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=155 DEFAULT CHARSET=latin1;
@@ -35,6 +37,80 @@ CREATE TABLE `device` (
 --
 -- Table structure for table `identity_provider`
 --
+
+DROP TABLE IF EXISTS `identity_provider`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `identity_provider` (
+  `_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` varchar(36) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `entity_id` varchar(50) NOT NULL,
+  `federation_id` varchar(50) NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`_id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `institution`
+--
+
+DROP TABLE IF EXISTS `institution`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `institution` (
+  `pk_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` varchar(36) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `description` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`pk_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10013 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `publisher`
+--
+
+DROP TABLE IF EXISTS `publisher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `publisher` (
+  `_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` varchar(36) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `status` varchar(15) NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`_id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `publisher_session`
+--
+
+DROP TABLE IF EXISTS `publisher_session`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `publisher_session` (
+  `_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` varchar(36) NOT NULL,
+  `local_id` varchar(45) NOT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `device_id` varchar(36) DEFAULT NULL,
+  `authenticated_by_id` varchar(36) DEFAULT NULL,
+  `publisher_id` varchar(36) DEFAULT NULL,
+  `last_active_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=135 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
